a947230c68aacaf17c9236541c3c09ad420d7c06
aleksnick <nikolaev@iprojects.ru>
2026-08-20 15:51:20 +0300

    feat: validate strategy configuration

--- src/HeadAndShoulders/strategy.ts
+++ src/HeadAndShoulders/strategy.ts
@@ -1,11 +1,16 @@
-import type { StrategyRegistryEntry } from "@tradejs/types";
+import { createStrategyConfigParser } from "@tradejs/strategy-kit/config";
+import type { ValidatedStrategyRegistryEntry } from "@tradejs/strategy-kit/config";
 import { config as DEFAULT_CONFIG, HeadAndShouldersConfig } from "./config";
 import { createHeadAndShouldersCore } from "./core";
 import { headAndShouldersManifest } from "./manifest";
 
-export const HeadAndShouldersStrategyDefinition: StrategyRegistryEntry<HeadAndShouldersConfig> =
+export const HeadAndShouldersStrategyDefinition: ValidatedStrategyRegistryEntry<HeadAndShouldersConfig> =
   {
     defaults: DEFAULT_CONFIG,
+    parseConfig: createStrategyConfigParser({
+      strategyName: "HeadAndShoulders",
+      defaults: DEFAULT_CONFIG,
+    }),
     createCore: createHeadAndShouldersCore,
     manifest: headAndShouldersManifest,
   };
